b4ca41fe929b21e7ec58b93505a6f5a463f370f9
Gauthmi <gauthamiv75@gmail.com>
2022-05-22 22:10:50 +0530

    handleWriteBlock function added

--- main.go
+++ main.go
@@ -20,6 +20,9 @@ type Block struct {
 	Hash      string
 	PrevHash  string
 }
+type Message struct {
+	BPM int
+}
 
 var Blockchain []Block
 
@@ -101,4 +104,28 @@ func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	io.WriteString(w, string(bytes))
+}
+func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
+	var m Message
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&m); err != nil {
+		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+		return
+	}
+	defer r.Body.Close()
+
+	newBlock, err := generateBlock(Blockchain[len(Blockchain)-1], m.BPM)
+	if err != nil {
+		respondWithJSON(w, r, http.StatusInternalServerError, m)
+		return
+	}
+	if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+		newBlockchain := append(Blockchain, newBlock)
+		replaceChain(newBlockchain)
+		spew.Dump(Blockchain)
+	}
+
+	respondWithJSON(w, r, http.StatusCreated, newBlock)
+
 }
